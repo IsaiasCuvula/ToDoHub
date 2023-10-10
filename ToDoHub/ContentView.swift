65686a1eb04ef03266b5683192bb9b6200a5fc05
Isaias Cuvula <68303716+IsaiasCuvula@users.noreply.github.com>
2023-10-10 04:23:07 +0300

    Initial Commit

---
+++ ToDoHub/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ToDoHub
+//
+//  Created by Isaias Cuvula on 10.10.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("ToDoHub")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ToDoHub/ToDoHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoHubApp.swift
+//  ToDoHub
+//
+//  Created by Isaias Cuvula on 10.10.23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
