4375c394719dfb5cd2c50e6067595d95e70a8573
Isaias Cuvula <bersyteinfo@gmail.com>
2023-10-12 03:57:41 +0300

    welcome view ui

---
+++ ToDoHub/Extensions/Colors.swift
@@ -0,0 +1,8 @@
+//
+//  Colors.swift
+//  ToDoHub
+//
+//  Created by Isaias Cuvula on 12.10.23.
+//
+
+import Foundation
---
+++ ToDoHub/Views/Auth/SignUpView.swift
@@ -0,0 +1,18 @@
+//
+//  SignUpView.swift
+//  ToDoHub
+//
+//  Created by Isaias Cuvula on 12.10.23.
+//
+
+import SwiftUI
+
+struct SignUpView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignUpView()
+}
